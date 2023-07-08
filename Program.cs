a606e878ee2f1cf2a430b1fdecbca5869912b6d2
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-07-08 15:42:58 +0530

    welcome to UC2

--- Program.cs
+++ Program.cs
@@ -6,21 +6,28 @@
         {
             Console.WriteLine("Welcome To Employee Wage Computation!");
 
-            int IS_FULL_TIME = 0;
-            Random random = new Random();
-            int empCheck = random.Next(1, 2);
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
 
+            int empHour = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            Console.WriteLine(empCheck);
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee Is Present");
+                empHour = 9;
             }
             else
             {
                 Console.WriteLine("Employee Is Absent");
+                empWage = 0;
             }
+            empWage += empHour * EMP_RATE_PER_HOUR;
+            Console.WriteLine("EmpWage :" + empWage);
+
         }
     }
-}
-
-
-
